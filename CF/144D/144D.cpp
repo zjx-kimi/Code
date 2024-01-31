5ac34f6a57d94c4bbd83e7baf82e661f4eb4904b
zjx-kimi <134782384+zjx-kimi@users.noreply.github.com>
2024-01-31 20:08:17 +0800

    updata 144D.cpp

--- CF/144D/144D.cpp
+++ CF/144D/144D.cpp
@@ -3,6 +3,7 @@
 #define pii pair<int, int>
 using namespace std;
 const int N = 1e5 + 10;
+int u, v, d;
 int x[N], y[N], w[N];
 int dis[N], n, s, m;
 vector<pii> edge[N];
@@ -12,13 +13,10 @@ signed main() {
     cin >> n >> m >> s;
     for (int i = 1; i <= n; i++) dis[i] = INT_MAX;
     for (int i = 1; i <= m; i++) {
-        int u, v, d;
         cin >> u >> v >> d;
         x[i] = u, y[i] = v, w[i] = d;
-        edge[u].push_back((node) {
-            v, d });
-        edge[v].push_back((node) {
-            u, d });
+        edge[u].push_back({v, d });
+        edge[v].push_back({u, d });
     }
     dis[s] = 0;
     q.push((node) {
@@ -43,16 +41,13 @@ signed main() {
     }
     for (int i = 1; i <= m; i++) {
         if (dis[x[i]] + w[i] - l + dis[y[i]] == l && dis[y[i]] < l && dis[x[i]] < l) {
-            //			cout<<"case 1: "<<i<<"\n";
             cnt++;
             continue;
         }
         if (dis[x[i]] + w[i] - l + dis[y[i]] > l && dis[x[i]] < l) {
-            //			cout<<"case 2: "<<i<<"\n";
             cnt++;
         }
         if (dis[x[i]] + w[i] - l + dis[y[i]] > l && dis[y[i]] < l) {
-            //			cout<<"case 3: "<<i<<"\n";
             cnt++;
         }
     }
