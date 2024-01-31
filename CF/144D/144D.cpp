2a3039ab80a4cd3abfd81a9de8cd46d54c60e88a
zjx-kimi <134782384+zjx-kimi@users.noreply.github.com>
2024-01-31 20:10:42 +0800

    updata 144D.cpp

--- CF/144D/144D.cpp
+++ CF/144D/144D.cpp
@@ -5,7 +5,7 @@ using namespace std;
 const int N = 1e5 + 10;
 int u, v, d;
 int x[N], y[N], w[N];
-int dis[N], n, s, m;
+int dis[N], n, s, m, l;
 vector<pii> edge[N];
 priority_queue<pii> q;
 bool v[N];
@@ -19,8 +19,7 @@ signed main() {
         edge[v].push_back({u, d });
     }
     dis[s] = 0;
-    q.push((node) {
-        s, 0 });
+    q.push({s, 0 });
     while (q.size()) {
         int u = q.top().to;
         q.pop();
@@ -29,16 +28,15 @@ signed main() {
         for (int i = 0; i < edge[u].size(); i++) {
             if (dis[edge[u][i].to] > dis[u] + edge[u][i].v) {
                 dis[edge[u][i].to] = dis[u] + edge[u][i].v;
-                q.push((node) {
-                    edge[u][i].to, dis[edge[u][i].to] });
+                q.push({edge[u][i].to, dis[edge[u][i].to] });
             }
         }
     }
-    int cnt = 0, l;
+    int cnt = 0;
     cin >> l;
-    for (int i = 1; i <= n; i++) {
+    for (int i = 1; i <= n; i++)
         cnt += (dis[i] == l);
-    }
+    
     for (int i = 1; i <= m; i++) {
         if (dis[x[i]] + w[i] - l + dis[y[i]] == l && dis[y[i]] < l && dis[x[i]] < l) {
             cnt++;
