010c68d77568236d742efc50b60786adbc1fd512
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-02-01 20:08:19 +0800

    updata 1922.cpp

--- CF/1922D/1922.cpp
+++ CF/1922D/1922.cpp
@@ -1,16 +1,18 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int N = 3e5 + 10;
 int t, n;
 struct node {
     int a, d;
     int pre, nxt;
-};
+} Arr[];
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
     cout.tie(0);
     cin >> t;
     while(t--) {
-        
+        cin >> n;
+        for (int i = 1; i < n; i++ 
     }
 }
