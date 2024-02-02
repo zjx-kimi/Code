1c8b90b33750857cae304e5354fbc6581bd4d5af
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-02-02 22:50:49 +0800

    updatas

--- CF/1787C/1787C.cpp
+++ CF/1787C/1787C.cpp
@@ -3,7 +3,7 @@ using namespace std;
 #define case true
 #define endl '\n'
 #define int long long
-const int N = 2e5 + 10;
+const int N = 2e1 + 10;
 int t = 1;
 int n, s;
 int arr[N], dp[N][2], minn[N], maxx[N];
@@ -22,7 +22,7 @@ signed main() {
         maxx[i] = s;
       } else {
         minn[i] = s;
-        maxx[i] = arr[i] - s;
+        maxx[i] = max(0LL, arr[i] - s);
       }
     }
     dp[2][0] = arr[1] * minn[2], dp[2][1] = arr[1] * maxx[2];
@@ -32,7 +32,9 @@ signed main() {
       dp[i][1] = min(dp[i - 1][0] + maxx[i - 1] * maxx[i],
                      dp[i - 1][1] + minn[i - 1] * maxx[i]);
     }
-    cout << dp[n - 1][];
+    cout << min(dp[n - 1][0] + maxx[n - 1] * arr[n],
+                dp[n - 1][1] + minn[n - 1] * arr[n])
+         << '\n';
   }
   return 0;
 }
