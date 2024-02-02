b7fad58bfaafa7e7d319a30c4ae9736ebe207841
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-02-02 22:07:20 +0800

    updatas

--- CF/1787C/1787C.cpp
+++ CF/1787C/1787C.cpp
@@ -6,22 +6,27 @@ using namespace std;
 const int N = 2e5 + 10;
 int t = 1;
 int n, s;
-int arr[N], dp[2][N];
+int arr[N], dp[2][N], minn[N], maxx[N];
 signed main() {
   ios::sync_with_stdio(false);
   cin.tie(0);
-  cout.tie(0);
+  cout.tie(0); 
   if (case)
     cin >> t;
   while (t--) {
     cin >> n >> s;
     for (int i = 1; i <= n; i++) {
-      cin >> a[i];
+      cin >> arr[i];
+      if (2 * s >= arr[i]) {
+        minn[i] = arr[i] - s;
+        maxx[i] = s;
+      } else {
+        minn[i] = s;
+        maxx[i] = arr[i] - s;
+      }
     }
-    for (int i = 1; i <= n; i++) {}
+    for (int i = 1; i <= n; i++) {
+    }
   }
   return 0;
 }
-/*
-- 列出
-* /
