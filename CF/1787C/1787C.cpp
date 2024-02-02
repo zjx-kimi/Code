3bbaa81f8128cf0184c3eda8ae269ec6a9286279
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-02-02 22:25:18 +0800

    updata

--- CF/1787C/1787C.cpp
+++ CF/1787C/1787C.cpp
@@ -25,9 +25,14 @@ signed main() {
         maxx[i] = arr[i] - s;
       }
     }
-	dp[2][1] = arr[1] * 
-    for (int i = 1; i <= n; i++) {
+    dp[2][0] = arr[1] * minn[2], dp[2][1] = arr[1] * maxx[2];
+    for (int i = 3; i < n; i++) {
+      dp[i][0] = min(dp[i - 1][0] + maxx[i - 1] * minn[i],
+                     dp[i - 1][1] + minn[i - 1] * minn[i]);
+      dp[i][1] = min(dp[i - 1][0] + maxx[i - 1] * maxx[i],
+                     dp[i - 1][1] + minn[i - 1] * maxx[i]);
     }
+    cout << dp[n - 1][];
   }
   return 0;
 }
