e32ea4604f190a841aa66abfbcca9effb38d4171
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-02-02 20:47:33 +0800

    Great some files

---
+++ CF/1787C/1787C.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define case true
+#define endl '\n'
+#define int long long
+const int N = 2e5 + 10;
+int t = 1;
+
+signed main(){
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	cout.tie(0);
+	if (case) 
+		cin >> t;
+	while (t--) {
+		
+	}
+	return 0;
+}
