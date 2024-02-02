9fe89f6aa98575ce35e5e1be49949115c618ab31
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-02-02 20:47:33 +0800

    Changes

---
+++ CF/1787C/1787C.cpp
@@ -0,0 +1,27 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define case true
+#define endl '\n'
+#define int long long
+const int N = 2e5 + 10;
+int t = 1;
+int n, s;
+int arr[N], dp[2][N];
+signed main() {
+  ios::sync_with_stdio(false);
+  cin.tie(0);
+  cout.tie(0);
+  if (case)
+    cin >> t;
+  while (t--) {
+    cin >> n >> s;
+    for (int i = 1; i <= n; i++) {
+      cin >> a[i];
+    }
+    for (int i = 1; i <= n; i++) {}
+  }
+  return 0;
+}
+/*
+- 列出
+* /
