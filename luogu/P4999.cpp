3955c2d10a7e2899b758b418b4c55b158453c6ba
zjx-kimi <zhongjiaxuankimi@qq.com>
2024-02-06 08:32:18 +0800

    AC P4999

--- luogu/P4999.cpp
+++ luogu/P4999.cpp
@@ -1,19 +1,42 @@
-
 #include <bits/stdc++.h>
 using namespace std;
 const int Mod = 1e9 + 7;
-int t, l, r;
-int dp[20][10];
-void dfs(int id, int num) {
-
-}
-int main()
+long long t, l, r, len;
+long long dp[20][200];
+int a[20];
+long long dfs(int id, int sum, bool limit)
 {
-    scanf("%d", &t);
+    if (id == 0)
+        return sum;
+    if (dp[id][sum] >= 0 && limit == 0)
+        return dp[id][sum];
+    int bound = limit ? a[id] : 9;
+    long long res = 0;
+    for (int i = 0; i <= bound; i++)
+    {  
+        res += dfs(id - 1, sum + i, limit && i == bound);
+        res %= Mod;
+    }
+    return limit == 0 ? dp[id][sum] = res : res;
+}
+long long solve(long long x)
+{
+    len = 0;
+    while (x)
+    {
+        a[++len] = x % 10;
+        x /= 10;
+    }
+    return dfs(len, 0, 1);
+}
+signed main()
+{
+    memset(dp, -1, sizeof(dp));
+    scanf("%lld", &t);
     while (t--)
     {
-        scanf("%d%d", &l, &r);
-        dfs()
+        scanf("%lld%lld", &l, &r);
+        printf("%lld\n", (solve(r) - solve(l - 1) + Mod) % Mod);
     }
     return 0;
 }
