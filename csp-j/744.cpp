dc9cf19aa378646b684a0467a3b7749983b1e745
kimi0705 <1345098180@qq.com>
2024-02-08 15:04:14 +0800

    updata

--- csp-j/744.cpp
+++ csp-j/744.cpp
@@ -1,9 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
+const int Mod = 10007;
 int n, ans, maxx;
 int u, v;
+int max1, max2 = 0;
+int t1, t2;
 vector<int> Edge[100010];
-int arr[100010]
+int arr[100010];
 int main() {
     ios::sync_with_stdio(false);
     cin.tie(0);
@@ -16,18 +19,16 @@ int main() {
     for (int i = 1; i <= n; i++)
         cin >> arr[i];
     for (int i = 1; i <= n; i++) {
-        int max1 = 0, max2 = 0;  
-        int t1 = 0, t2 = 0;      
         for (int j : Edge[i]) {
             if (w[j] > max1)
                 max2 = max1, max1 = w[j];
             else if (w[j] > max2)
                 max2 = w[j];
-            t1 = (t1 + w[j]) % 10007;
-            t2 = (t2 + w[j] * w[j]) % 10007;
+            t1 = (t1 + w[j]) % Mod;
+            t2 = (t2 + w[j] * w[j]) % Mod;
         }
-        t1 = t1 * t1 % 10007;
-        ans = (ans + t1 + 10007 - t2) % 10007;
+        t1 = t1 * t1 % Mod;
+        ans = (ans + t1 + Mod - t2) % Mod;
         if (maxx < max1 * max2)
             maxx = max1 * max2;
     }
